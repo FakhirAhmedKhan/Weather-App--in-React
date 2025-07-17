97552bf35e4e9b00fcf8bab28651bf2bea53a8d3
Fakhir Ahmed Khan <fakhirahmed898@gmail.com>
2025-07-18 01:49:39 +0400

    fix

--- my-app/vite.config.js
+++ my-app/vite.config.js
@@ -5,5 +5,6 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  // eslint-disable-next-line no-undef
   base: process.env.VITE_BASE || "/Weather-App--in-React/",
 });
